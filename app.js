5a05e1250274d215e877479bf9e6aa73997f5949
‘peter-robert-cole’ <petercole3@gmail.com>
2020-03-02 15:03:09 -0800

    progress with reading files

--- app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const files = require('./lib/files');
-
-const inputFileName = process.argv.slice(2).shift();
-
-files.alterFile(inputFileName);
--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 'use strict';
 
+const files = require('./lib/files');
+
+const inputFieldName = process.argv[2];
+const buffer = process.argv[3];
+
+files.loadFile(inputFieldName, buffer);
